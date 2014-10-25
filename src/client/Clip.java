c27135a2990fba1d54da8919b4ab14647e32bd50
Brandon Davis <bdavis@redspin.net>
2014-10-25 19:06:46 -0400

    Server changes

--- src/client/Clip.java
+++ src/client/Clip.java
@@ -2,7 +2,7 @@ package client;
 
 public class Clip {
 	public static void main(String[] args){
-		//WOHOO
+		
 	}
 
 }
--- src/server/Main.java
+++ src/server/Main.java
@@ -7,11 +7,12 @@ public class Main {
 	public static void main(String args[]) throws IOException{
 		ServerSocket listener = new ServerSocket(9090);
 		try {
-			Handler s = new Handler(listener.accept());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+            while (true) {
+                new Handler(listener.accept()).start();
+            }
+        } finally {
+            listener.close();
+        }
 	}
 
 }
