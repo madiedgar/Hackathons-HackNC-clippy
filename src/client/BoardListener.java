24f2509bbd2301ddf570b83ad7dc2135a9a0f85f
Madison Pfaff <mpfaff@live.unc.edu>
2014-10-25 20:18:46 -0400

    Board Listener

---
+++ src/client/BoardListener.java
@@ -0,0 +1,21 @@
+package client;
+
+import java.awt.*;
+import java.awt.datatransfer.*;
+
+public class BoardListener extends ClipboardHandler{
+
+//	public static void main(String[] args) throws Exception {
+//		Toolkit.getDefaultToolkit().getSystemClipboard()
+//		.addFlavorListener(new FlavorListener() {
+//			@Override
+//			public void flavorsChanged(FlavorEvent e) {
+//				System.out.println("changed!!! " + e.getSource() + " "
+//						+ e.toString());
+//			}
+//		});
+//		Thread.sleep(1000000L);
+//	}
+
+
+}
