83fc17c9a5b3c10b57fec6ad4f01df09ab2a9c75
Alexandra Harrison <apharri3@live.unc.edu>
2014-10-25 20:34:12 -0400

    Added Try/Catch

--- src/client/BoardListener.java
+++ src/client/BoardListener.java
@@ -5,17 +5,23 @@ import java.awt.datatransfer.*;
 
 public class BoardListener extends ClipboardHandler{
 
-//	public static void main(String[] args) throws Exception {
-//		Toolkit.getDefaultToolkit().getSystemClipboard()
-//		.addFlavorListener(new FlavorListener() {
-//			@Override
-//			public void flavorsChanged(FlavorEvent e) {
-//				System.out.println("changed!!! " + e.getSource() + " "
-//						+ e.toString());
-//			}
-//		});
-//		Thread.sleep(1000000L);
-//	}
+	public static void main(String[] args)  {
+		Toolkit.getDefaultToolkit().getSystemClipboard()
+		.addFlavorListener(new FlavorListener() {
+			@Override
+			public void flavorsChanged(FlavorEvent e) {
+				System.out.println("changed!!! " + e.getSource() + " "
+						+ e.toString());
+			}
+		}
+		);
+		try {
+			Thread.sleep(1000000L);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
 
 
 }
